f7a0a01fb656b34362655172f05a1d9eec15ad1b
xiaohai-huang <jeromehuang18@gmail.com>
2023-04-26 16:40:55 +0800

    升级到v6.0

--- src/components/MimiBase/index.jsx
+++ src/components/MimiBase/index.jsx
@@ -24,7 +24,7 @@ function MimiBase() {
   return (
     <div style={{ padding: "0 12px" }}>
       <h1 style={{ textAlign: "center", marginBottom: "8px" }}>
-        秘密保存基地 <i>V5.0</i>
+        秘密保存基地 <i>V6.0</i>
       </h1>
       <p style={{ fontSize: "28px" }}>
         欢迎{" "}
