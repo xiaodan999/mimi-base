0d58651a775da6c39212506d05db82fe93da1df9
xiaohai-huang <jeromehuang18@gmail.com>
2023-04-28 14:18:56 +0800

    run prettier on vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
   plugins: [
     react(),
     // put it the last one
     visualizer({
       emitFile: true,
-      template: 'sunburst',
+      template: "treemap",
     }),
   ],
-})
+});
