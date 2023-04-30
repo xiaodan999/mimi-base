d3a3246bd5ab756bb4061994ffa42a1138fc1c8a
xiaohai-huang <jeromehuang18@gmail.com>
2023-04-30 23:41:37 +0800

    chore: move react-query to main layout

--- src/app/(protected)/(main)/layout.jsx
+++ src/app/(protected)/(main)/layout.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+
 import NavButtons from "../../../components/NavButtons";
 
 import styles from "./layout.module.css";
 
+const queryClient = new QueryClient();
+
 function Layout() {
   return (
-    <div className={styles.layout}>
-      <Outlet />
-      <NavButtons />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className={styles.layout}>
+        <Outlet />
+        <NavButtons />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { AuthProvider } from "../contexts/AuthContext";
-
-const queryClient = new QueryClient();
 
 // The root layout for the whole app
 function Layout() {
   return (
     <div className="root-layout" style={{ height: "100%" }}>
       <AuthProvider>
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-        </QueryClientProvider>
+        <Outlet />
       </AuthProvider>
     </div>
   );
