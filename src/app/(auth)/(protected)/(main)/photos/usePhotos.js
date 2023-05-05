3fe0083a89d67b60bc9636b8274b4e3023d137a1
xiaohai-huang <jeromehuang18@gmail.com>
2023-05-05 15:11:24 +0800

    refactor: extract fetchPhotos and fix nextCursor
    - Extract supabase code from getPhotos function into a separate fetchPhotos function
    - Fix calculation of nextCursor value in getPhotos function to use limit argument instead of hardcoded value
    - Use optional chaining and nullish coalescing operator to set nextCursor value

--- src/app/(auth)/(protected)/(main)/photos/usePhotos.js
+++ src/app/(auth)/(protected)/(main)/photos/usePhotos.js
@@ -12,40 +12,48 @@ function transform(data) {
   return transformed;
 }
 
-async function getPhotos(cursor, limit = 10) {
-  if (cursor === "initial") {
-    const { data, error } = await supabase
-      .from("tu-pian-xin-xi")
-      .select("user_id,photo,id,created_at,users(user_name)")
-      .order("created_at", { ascending: false })
-      .limit(limit + 1);
-    if (error) throw error;
-    return { photos: transform(data.slice(0, 10)), nextCursor: data.at(-1).created_at, limit };
-  } else {
-    const { data, error } = await supabase
-      .from("tu-pian-xin-xi")
-      .select("user_id,photo,id,created_at,users(user_name)")
-      .lte("created_at", cursor)
-      .order("created_at", { ascending: false })
-      .limit(limit + 1);
-    if (error) throw error;
+async function fetchPhotos(cursor, limit) {
+  // Fetch photos from the database using supabase
+  let query = supabase
+    .from("tu-pian-xin-xi")
+    .select("user_id,photo,id,created_at,users(user_name)")
+    .order("created_at", { ascending: false })
+    .limit(limit + 1);
 
-    return {
-      photos: transform(data.slice(0, 10)),
-      nextCursor: data.length < limit ? null : data.at(-1).created_at,
-      limit,
-    };
+  if (cursor) {
+    query = query.lte("created_at", cursor);
   }
+
+  const { data, error } = await query;
+
+  if (error) throw error;
+  return data;
+}
+
+async function getPhotos(cursor, limit = 10) {
+  let data;
+  if (cursor === "initial") {
+    // If cursor is "initial", fetch the first set of photos
+    data = await fetchPhotos(null, limit);
+  } else {
+    // If cursor is not "initial", fetch the next set of photos
+    data = await fetchPhotos(cursor, limit);
+  }
+
+  const photos = transform(data.slice(0, limit)); // Transform the first limit photos in the data array
+  const nextCursor = data[limit]?.created_at ?? null; // Set the value of nextCursor
+
+  return { photos, nextCursor, limit }; // Return an object containing the photos array, nextCursor value and limit
 }
 
 export default function usePhotos() {
   return useInfiniteQuery({
     queryKey: ["photos"],
     queryFn: ({ pageParam = "initial" }) => {
-      return getPhotos(pageParam);
+      return getPhotos(pageParam); // Call the getPhotos function with the pageParam as the cursor argument
     },
     getNextPageParam: (lastpage) => {
-      return lastpage.nextCursor;
+      return lastpage.nextCursor; // Return the value of nextCursor from the last page of data
     },
   });
 }
