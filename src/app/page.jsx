f7d3bf614f36fd870f197988e44e6fd32e802ba1
xiaohai-huang <jeromehuang18@gmail.com>
2023-05-05 16:50:28 +0800

    style: beautify the index page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,28 @@
 import { useNavigate } from "react-router-dom";
-import { Button } from "antd-mobile";
+import { AutoCenter, Button } from "antd-mobile";
+
+import Logo from "./logo512.png";
 
 function Page() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>秘密基地</h1>
-      <p>这是一个任何人都可以访问的页面, 无需登入就可以访问哦</p>
-      <Button onClick={() => navigate("/home")}>进入秘密基地</Button>
+    <div style={{ height: "100%", padding: "1rem", display: "flex", flexDirection: "column" }}>
+      <AutoCenter>
+        <h1 style={{ marginBottom: "1rem" }}>秘密基地</h1>
+      </AutoCenter>
+      <AutoCenter>
+        <img src={Logo} title="logo" alt="logo" width="300px" height="300px" />
+      </AutoCenter>
+
+      <AutoCenter>
+        <p>这是一个任何人都可以访问的页面, 无需登入就可以访问哦</p>
+      </AutoCenter>
+      <div style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Button color="primary" size="large" onClick={() => navigate("/home")}>
+          进入秘密基地
+        </Button>
+      </div>
     </div>
   );
 }
