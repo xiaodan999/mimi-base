d7eafc82fae9088d17296bb98bcfb4ff69085c2b
xiaohai-huang <jeromehuang18@gmail.com>
2023-05-14 00:53:48 +0800

    style: adjust the style of the chat page

--- src/app/(auth)/(protected)/(main)/chat/page.jsx
+++ src/app/(auth)/(protected)/(main)/chat/page.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Button } from "antd-mobile";
+import { AutoCenter, Button, TextArea } from "antd-mobile";
 
 import XiaohaiTab from "@src/components/XiaohaiTab";
 import { useUser } from "@src/contexts/AuthContext";
 import supabase from "@src/supabase-client/supabase";
+
+import styles from "./page.module.css";
 
 export default function Page() {
   const [user] = useUser();
@@ -11,31 +13,33 @@ export default function Page() {
   const [submitting, setSubmitting] = useState(false);
 
   return (
-    <div
-      style={{
-        padding: "0 10px",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <h1 style={{ fontSize: "32px", textAlign: "center", marginTop: "10px", marginBottom: "5px" }}>
-        聊天基地
-      </h1>
-      <section style={{ overflow: "hidden", flex: 1 }}>
+    <div className={styles.page}>
+      <AutoCenter>
+        <h1>聊天基地</h1>
+      </AutoCenter>
+      <section className={styles.messages}>
         <XiaohaiTab />
       </section>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <textarea
-          placeholder="请输入内容"
-          value={mimi}
-          onChange={(e) => {
-            setMimi(e.target.value);
-          }}
-          cols={60}
-        ></textarea>
+      <div className={styles.textInput}>
+        <div className={styles.wrapper}>
+          <TextArea
+            style={{
+              backgroundColor: "var(--accent-color)",
+              "--placeholder-color": "var(--text-color)",
+            }}
+            placeholder="请输入内容"
+            value={mimi}
+            onChange={(value) => {
+              setMimi(value);
+            }}
+            cols={60}
+          />
+        </div>
         <Button
+          className={styles.button}
+          shape="rounded"
           color="success"
+          loading={submitting}
           disabled={mimi.length === 0 || /^ *$/.test(mimi) || submitting}
           onClick={async () => {
             setSubmitting(true);
@@ -43,14 +47,8 @@ export default function Page() {
             setMimi("");
             setSubmitting(false);
           }}
-          style={{
-            width: "120px",
-            padding: "10px",
-            borderRadius: "10px",
-            fontSize: "14px",
-          }}
         >
-          添加 {submitting ? "..." : ""}
+          添加
         </Button>
       </div>
     </div>
