9db408840a2c5da114f3e079f0346e5d4b708310
xiaodan999 <673414237@qq.com>
2023-05-17 21:37:15 +0800

    resize TouXiang's source image

--- src/components/TouXiang/index.jsx
+++ src/components/TouXiang/index.jsx
@@ -1,9 +1,11 @@
-export default function TouXiang({ size, touXiangUrl, circleUrl }) {
+import getResizedUrl from "@src/utils/getResizedUrl";
+
+export default function TouXiang({ size, touXiangUrl, circleUrl, style = {} }) {
   return (
-    <div style={{ position: "relative", width: size + "px", height: size + "px" }}>
+    <div style={{ position: "relative", width: size + "px", height: size + "px", ...style }}>
       <img
         style={{ width: "100%", height: "100%", borderRadius: "50%", objectFit: "cover" }}
-        src={touXiangUrl}
+        src={getResizedUrl({ url: touXiangUrl, width: size * 2, height: size * 2, quality: 1 })}
         alt="touxiang"
       />
       <img
@@ -13,7 +15,7 @@ export default function TouXiang({ size, touXiangUrl, circleUrl }) {
           position: "absolute",
           left: 0,
         }}
-        src={circleUrl}
+        src={getResizedUrl({ url: circleUrl, width: size * 2, height: size * 2, quality: 1 })}
         alt="circle"
       />
     </div>
