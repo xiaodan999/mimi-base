671dabe85d19c767041f9ec1187aab7f48222f75
xiaohai-huang <jeromehuang18@gmail.com>
2023-05-17 22:55:30 -0600

    fix: resize avatar to 512x512 by default

--- src/components/TouXiang/index.jsx
+++ src/components/TouXiang/index.jsx
@@ -5,7 +5,7 @@ export default function TouXiang({ size, touXiangUrl, circleUrl, style = {} }) {
     <div style={{ position: "relative", width: size + "px", height: size + "px", ...style }}>
       <img
         style={{ width: "100%", height: "100%", borderRadius: "50%", objectFit: "cover" }}
-        src={getResizedUrl({ url: touXiangUrl, width: size * 2, height: size * 2, quality: 1 })}
+        src={getResizedUrl({ url: touXiangUrl, width: 512, height: 512, quality: 1 })}
         alt="touxiang"
       />
       <img
@@ -15,7 +15,7 @@ export default function TouXiang({ size, touXiangUrl, circleUrl, style = {} }) {
           position: "absolute",
           left: 0,
         }}
-        src={getResizedUrl({ url: circleUrl, width: size * 2, height: size * 2, quality: 1 })}
+        src={getResizedUrl({ url: circleUrl, width: 512, height: 512, quality: 1 })}
         alt="circle"
       />
     </div>
