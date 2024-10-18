4a383709a38ad59b0bf1cad0743acea6ba8afa17
xiaodan999 <673414237@qq.com>
2024-10-18 14:25:22 +0800

    chore: migrate old code

---
+++ lib/toast-promise.ts
@@ -0,0 +1,42 @@
+import { toast } from "sonner";
+type PromiseT<Data = any> = Promise<Data> | (() => Promise<Data>);
+type PromiseTResult<Data = any> =
+	| string
+	| React.ReactNode
+	| ((
+			data: Data,
+	  ) => React.ReactNode | string | Promise<React.ReactNode | string>);
+type PromiseData<ToastData = any> = {
+	loading?: string | React.ReactNode;
+	success?: PromiseTResult<ToastData>;
+	error?: PromiseTResult;
+	description?: PromiseTResult;
+	finally?: () => void | Promise<void>;
+};
+
+export function toastPromise<Data>(
+	promise: PromiseT<Data>,
+	data: PromiseData<Data>,
+): Promise<Data> {
+	return new Promise((resolve, reject) => {
+		toast.promise(promise, {
+			description: data.description,
+			loading: data?.loading,
+			success: (payload) => {
+				resolve(payload);
+				if (typeof data.success === "function") {
+					return data.success(payload);
+				}
+				return data.success;
+			},
+			error: (err) => {
+				reject(err);
+				if (typeof data.error === "function") {
+					return data.error(err);
+				}
+
+				return data.error;
+			},
+		});
+	});
+}
--- src/supabase-client/supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient("supabaseUrl", "supabaseKey");
-export default supabase;
