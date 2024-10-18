d31a8d406b6c0befd50e068fa05126dbd426f7c4
xiaodan999 <673414237@qq.com>
2024-10-18 10:09:18 +0800

    chore: add tanstack router dev tools

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,10 +1,23 @@
 // app/routes/__root.tsx
+import ENV from "@/lib/env";
 import { createRootRoute } from "@tanstack/react-router";
 
 import { Outlet, ScrollRestoration } from "@tanstack/react-router";
 import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
 import appCss from "globals.css?url";
-import type * as React from "react";
+import { Suspense, lazy } from "react";
+
+const TanStackRouterDevtools =
+	ENV.ENV === "production"
+		? () => null // Render nothing in production
+		: lazy(() =>
+				// Lazy load in development
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools,
+					// For Embedded Mode
+					// default: res.TanStackRouterDevtoolsPanel
+				})),
+			);
 
 export const Route = createRootRoute({
 	meta: () => [
@@ -38,6 +51,9 @@ function RootComponent() {
 	return (
 		<RootDocument>
 			<Outlet />
+			<Suspense>
+				<TanStackRouterDevtools />
+			</Suspense>
 		</RootDocument>
 	);
 }
