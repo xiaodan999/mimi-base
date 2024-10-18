9cf8c114cdfadd6a370379af709a3d2160e0d6aa
xiaodan999 <673414237@qq.com>
2024-10-18 18:10:16 +0800

    chore: remove unused code

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,17 @@
 // app.config.ts
 import { defineConfig } from "@tanstack/start/config";
-import path from "path";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    preset: "node-server",
-  },
-  vite: {
-    plugins: [
-      // this is the plugin that enables path aliases
-      viteTsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-    ],
-  },
+	server: {
+		preset: "node-server",
+	},
+	vite: {
+		plugins: [
+			// this is the plugin that enables path aliases
+			viteTsConfigPaths({
+				projects: ["./tsconfig.json"],
+			}),
+		],
+	},
 });
