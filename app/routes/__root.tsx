87d471df754ece73e959f8304b2a737ad5ffa8e1
xiaodan999 <673414237@qq.com>
2024-10-19 14:11:41 +0800

    fix: only load dev tool during development

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,7 +1,20 @@
 import { Toaster } from "@/components/ui/sonner";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { lazy } from "react";
+
 import "globals.css";
+
+const TanStackRouterDevtools =
+	process.env.NODE_ENV === "production"
+		? () => null // Render nothing in production
+		: lazy(() =>
+				// Lazy load in development
+				import("@tanstack/router-devtools").then((res) => ({
+					default: res.TanStackRouterDevtools,
+					// For Embedded Mode
+					// default: res.TanStackRouterDevtoolsPanel
+				})),
+			);
 
 export const Route = createRootRoute({
 	component: () => (
