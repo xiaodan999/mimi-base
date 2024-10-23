f2d3a27de4dfe5ade009380eb83426bafca1f2c4
xiaohai-huang <jeromehuang18@gmail.com>
2024-10-23 23:30:19 +0800

    format code

--- app/routes/_protected.tsx
+++ app/routes/_protected.tsx
@@ -1,37 +1,37 @@
+import { useEffect } from "react";
+import {
+    Outlet,
+    createFileRoute,
+    redirect,
+    useRouter,
+} from "@tanstack/react-router";
+
 import LoadingPage from "@/components/LoadingPage";
 import { useAuth } from "@/lib/auth";
-import {
-	Outlet,
-	createFileRoute,
-	redirect,
-	useRouter,
-} from "@tanstack/react-router";
-import { useEffect } from "react";
 
 export const Route = createFileRoute("/_protected")({
-	beforeLoad: async ({ context, location }) => {
-		console.log("beforeLoad in /_protected", context.auth, location.pathname);
-		if (context.auth.loading) return;
+    beforeLoad: async ({ context, location }) => {
+        if (context.auth.loading) return;
 
-		if (!context.auth.isAuthenticated) {
-			throw redirect({
-				to: "/login",
-				search: {
-					redirect: location.href,
-				},
-			});
-		}
-	},
-	component: () => {
-		const router = useRouter();
-		const { loading, isAuthenticated } = useAuth();
+        if (!context.auth.isAuthenticated) {
+            throw redirect({
+                to: "/login",
+                search: {
+                    redirect: location.href,
+                },
+            });
+        }
+    },
+    component: function Component() {
+        const router = useRouter();
+        const { loading, isAuthenticated } = useAuth();
 
-		useEffect(() => {
-			if (!loading) router.invalidate();
-		}, [loading, router]);
+        useEffect(() => {
+            if (!loading) router.invalidate();
+        }, [loading, router]);
 
-		const showLoading = !isAuthenticated && loading;
+        const showLoading = !isAuthenticated && loading;
 
-		return showLoading ? <LoadingPage /> : <Outlet />;
-	},
+        return showLoading ? <LoadingPage /> : <Outlet />;
+    },
 });
