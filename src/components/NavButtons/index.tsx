97e9900d1dcec35f7e0447795ba6f385ebc821e0
xiaohai-huang <jeromehuang18@gmail.com>
2024-10-30 20:17:02 +0800

    style: use flex to structure the app

--- src/components/NavButtons/index.tsx
+++ src/components/NavButtons/index.tsx
@@ -1,33 +1,32 @@
+import { Link } from "@tanstack/react-router";
 
+import styles from "./buttons.module.css";
 import ChatIcon from "./icons/chat.png";
 import PhotoIcon from "./icons/photo.png";
 import VideoIcon from "./icons/video.png";
 import WalletIcon from "./icons/wallet.png";
 
-import { Link } from "@tanstack/react-router";
-import styles from "./buttons.module.css";
-
 const routes = [
-  { name: "视频基地", path: "/home", icon: VideoIcon },
-  { name: "照片基地", path: "/photos", icon: PhotoIcon },
-  { name: "聊天基地", path: "/chat", icon: ChatIcon },
-  { name: "记账基地", path: "/jizhang", icon: WalletIcon },
-];
+    { name: "视频基地", path: "/home", icon: VideoIcon },
+    { name: "照片基地", path: "/photos", icon: PhotoIcon },
+    { name: "聊天基地", path: "/chat", icon: ChatIcon },
+    { name: "记账基地", path: "/jizhang", icon: WalletIcon },
+] as const;
 
 export default function NavButtons() {
-  return (
-    <div className={styles.navBar}>
-      {routes.map((route) => (
-        <Link className={styles.link} key={route.path} to={route.path}>
-          {({ isActive }) => (
-            <img
-              className={`${styles.icon} ${isActive ? styles.active : ""}`}
-              src={route.icon}
-              alt={route.name}
-            />
-          )}
-        </Link>
-      ))}
-    </div>
-  );
+    return (
+        <div className={styles.navBar}>
+            {routes.map((route) => (
+                <Link className={styles.link} key={route.path} to={route.path}>
+                    {({ isActive }) => (
+                        <img
+                            className={`${styles.icon} ${isActive ? styles.active : ""}`}
+                            src={route.icon}
+                            alt={route.name}
+                        />
+                    )}
+                </Link>
+            ))}
+        </div>
+    );
 }
