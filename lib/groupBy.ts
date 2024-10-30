b149c56ce93563637fb34d8a9981861972854758
xiaohai-huang <jeromehuang18@gmail.com>
2024-10-30 22:36:39 +0800

    refactor: convert to ts

--- lib/groupBy.ts
+++ lib/groupBy.ts
@@ -11,16 +11,19 @@
  */
 //export function groupBy<K, V>(list: Array<V>, keyGetter: (input: V) => K): Map<K, Array<V>> {
 //    const map = new Map<K, Array<V>>();
-export default function groupBy(list, keyGetter) {
-  const map = new Map();
-  list.forEach((item) => {
-    const key = keyGetter(item);
-    const collection = map.get(key);
-    if (!collection) {
-      map.set(key, [item]);
-    } else {
-      collection.push(item);
-    }
-  });
-  return map;
+export default function groupBy<K, V>(
+    list: Array<V>,
+    keyGetter: (input: V) => K,
+): Map<K, Array<V>> {
+    const map = new Map();
+    list.forEach((item) => {
+        const key = keyGetter(item);
+        const collection = map.get(key);
+        if (!collection) {
+            map.set(key, [item]);
+        } else {
+            collection.push(item);
+        }
+    });
+    return map;
 }
